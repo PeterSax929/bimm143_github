b7c5d48454e1b5e34046fd7c06bceaa1599814be
Peter Sax <psax@ucsd.edu>
2025-05-20 11:56:01 -0700

    Adding Class4 and Class6

---
+++ class04/class04.R
@@ -0,0 +1,7 @@
+x <- 1:50
+plot(x)
+plot(x, sin(x))
+plot(x, sin(x), typ="l")
+plot(x, sin(x), typ="l", col="orange")
+plot(x, sin(x), typ="l", col="orange", lwd=3)
+plot(x, sin(x), typ="l", col="orange", lwd=3, xlab="Silly x axis", ylab="Sensible y axis")
